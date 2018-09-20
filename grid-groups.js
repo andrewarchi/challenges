dcf0ec64c0961598b8f4b26d4648601998b70898
Andrew Archibald <andrew@aarchibald.com>
2018-09-20 00:40:05 -0600

    grid: Adjust position variable names

--- grid-groups.js
+++ grid-groups.js
@@ -1,9 +1,9 @@
 function getGroups(grid) {
   const visited = [...Array(grid.length)].map(row => Array(grid[0].length).fill(false));
   const groups = [];
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[0].length; x++) {
-      const group = getGroup(x, y, grid, visited, []);
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const group = getGroup(i, j, grid, visited, []);
       if (group.length) {
         groups.push(group);
       }
@@ -12,23 +12,23 @@ function getGroups(grid) {
   return groups;
 }
 
-function getGroup(x, y, grid, visited, group) {
-  if (x < 0 || y < 0 || x >= grid[0].length || y >= grid.length) {
+function getGroup(i, j, grid, visited, group) {
+  if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
     return group;
   }
-  if (!grid[y][x] || visited[y][x]) {
+  if (!grid[i][j] || visited[i][j]) {
     return group;
   }
-  visited[y][x] = true;
-  group.push([x, y]);
-  getGroup(x-1, y-1, grid, visited, group);
-  getGroup(x,   y-1, grid, visited, group);
-  getGroup(x+1, y-1, grid, visited, group);
-  getGroup(x-1, y,   grid, visited, group);
-  getGroup(x+1, y,   grid, visited, group);
-  getGroup(x-1, y+1, grid, visited, group);
-  getGroup(x,   y+1, grid, visited, group);
-  getGroup(x+1, y+1, grid, visited, group);
+  visited[i][j] = true;
+  group.push([i, j]);
+  getGroup(i-1, j-1, grid, visited, group);
+  getGroup(i-1, j,   grid, visited, group);
+  getGroup(i-1, j+1, grid, visited, group);
+  getGroup(i,   j-1, grid, visited, group);
+  getGroup(i,   j+1, grid, visited, group);
+  getGroup(i+1, j-1, grid, visited, group);
+  getGroup(i+1, j,   grid, visited, group);
+  getGroup(i+1, j+1, grid, visited, group);
   return group;
 }
 
