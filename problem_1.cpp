3605bb7a3e49aaf3bea1c90b48425af47dca6ee7
Andrew Archibald <andrew@aarchibald.com>
2018-12-18 20:56:14 -0700

    euler: Create helper function for 1

--- problem_1.cpp
+++ problem_1.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-int problem1(unsigned int max) {
-  const unsigned int n3 = (max - 1) / 3;
-  const unsigned int n5 = (max - 1) / 5;
-  const unsigned int n15 = (max - 1) / 15;
-  return (
-    3 * n3 * (n3 + 1)
-    + 5 * n5 * (n5 + 1)
-    - 15 * n15 * (n15 + 1)
-  ) / 2;
+int problem1_s(unsigned int n, unsigned int d) {
+  unsigned int m = (n - 1) / d;
+  return d * m * (m + 1) / 2;
+}
+
+int problem1(unsigned int n) {
+  return problem1_s(n, 3) + problem1_s(n, 5) - problem1_s(n, 15);
 }
 
 int main() {
