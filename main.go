d4df043c7b1c8a27417e878d1557029508ca809c
Andrew Archibald <andrew@aarchibald.com>
2019-06-15 16:42:19 -0700

    fib: Timed solution in 1h4m

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/big"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func fib(num chan *big.Int) {
+	a := new(big.Int).SetUint64(0)
+	b := new(big.Int).SetUint64(1)
+	for {
+		num <- a
+		a, b = b, new(big.Int).Add(a, b)
+	}
+}
+
+func fillFib(n int64, cur chan *big.Int, nums []*big.Int) []*big.Int {
+	for i := int64(len(nums)); i < n; i++ {
+		nums = append(nums, <-cur)
+	}
+	return nums
+}
+
+type fibResponse struct {
+	Nums []*big.Int `json:"nums"`
+	Err  error      `json:"error"`
+}
+
+func serveFib() func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	var nums []*big.Int
+	cur := make(chan *big.Int)
+	go fib(cur)
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		nParam := p.ByName("n")
+		n, err := strconv.ParseInt(nParam, 10, 64)
+		if err != nil {
+			writeResponse(w, nil, fmt.Errorf("n must be integer, got %s", nParam))
+			return
+		}
+		if n < 0 {
+			writeResponse(w, nil, fmt.Errorf("n must be positive, got %d", n))
+			return
+		}
+		nums = fillFib(n, cur, nums)
+		writeResponse(w, nums[:n-1], nil)
+	}
+}
+
+func writeResponse(w http.ResponseWriter, nums []*big.Int, err error) {
+	b, err := json.Marshal(&fibResponse{nums, err})
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write(b)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/api/fibonacci/:n", serveFib())
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
